1d527eb872427f2f6d9558b4c2ad30f1c09eb1b4
Theagarajan <theagarajant@gmail.com>
2023-03-17 20:45:59 +0530

    Update script.js
    add more sounds

--- script.js
+++ script.js
@@ -14,7 +14,21 @@ const minimalPairs = [
   { word1: { text: 'tā', audio: 'audio/tā.mp3' }, word2: { text: 'tá', audio: 'audio/tá.mp3' } },
   { word1: { text: 'tǎ', audio: 'audio/tǎ.mp3' }, word2: { text: 'tà', audio: 'audio/tà.mp3' } },
   { word1: { text: 'nā', audio: 'audio/nā.mp3' }, word2: { text: 'ná', audio: 'audio/ná.mp3' } },
-  { word1: { text: 'nǎ', audio: 'audio/nǎ.mp3' }, word2: { text: 'nà', audio: 'audio/nà.mp3' } }
+  { word1: { text: 'nǎ', audio: 'audio/nǎ.mp3' }, word2: { text: 'nà', audio: 'audio/nà.mp3' } },
+  { word1: { text: 'lā', audio: 'audio/lā.mp3' }, word2: { text: 'lá', audio: 'audio/lá.mp3' } },
+  { word1: { text: 'lǎ', audio: 'audio/lǎ.mp3' }, word2: { text: 'là', audio: 'audio/là.mp3' } },
+  { word1: { text: 'gū', audio: 'audio/gū.mp3' }, word2: { text: 'gú', audio: 'audio/gú.mp3' } },
+  { word1: { text: 'gǔ', audio: 'audio/gǔ.mp3' }, word2: { text: 'gù', audio: 'audio/gù.mp3' } },
+  { word1: { text: 'kū', audio: 'audio/kū.mp3' }, word2: { text: 'kú', audio: 'audio/kú.mp3' } },
+  { word1: { text: 'kǔ', audio: 'audio/kǔ.mp3' }, word2: { text: 'kù', audio: 'audio/kù.mp3' } },
+  { word1: { text: 'hā', audio: 'audio/hā.mp3' }, word2: { text: 'há', audio: 'audio/há.mp3' } },
+  { word1: { text: 'hǎ', audio: 'audio/hǎ.mp3' }, word2: { text: 'hà', audio: 'audio/hà.mp3' } },
+  { word1: { text: 'jī', audio: 'audio/jī.mp3' }, word2: { text: 'jí', audio: 'audio/jí.mp3' } },
+  { word1: { text: 'jǐ', audio: 'audio/jǐ.mp3' }, word2: { text: 'jì', audio: 'audio/jì.mp3' } },
+  { word1: { text: 'qī', audio: 'audio/qī.mp3' }, word2: { text: 'qí', audio: 'audio/qí.mp3' } },
+  { word1: { text: 'qǐ', audio: 'audio/qǐ.mp3' }, word2: { text: 'qì', audio: 'audio/qì.mp3' } },
+  { word1: { text: 'xī', audio: 'audio/xī.mp3' }, word2: { text: 'xí', audio: 'audio/xí.mp3' } },
+  { word1: { text: 'xǐ', audio: 'audio/xǐ.mp3' }, word2: { text: 'xì', audio: 'audio/xì.mp3' } },
 ];
 
 let currentPair;
